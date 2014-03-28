e199f002dfccbe181225d32b1c2e129a5e4047f0
Alec Benzer <alecbenzer@gmail.com>
2014-03-28 12:30:32 -0500

    made a separate FifoMulticastChannel class

---
+++ fifo_multicast_channel.py
@@ -0,0 +1,44 @@
+import threading
+
+from Queue import Queue
+from collections import namedtuple
+from reliable_channel import ReliableChannel
+
+Message = namedtuple('Message', ['seq', 'data', 'id'])
+
+
+class FifoMulticastChannel:
+
+    def __init__(self, reliable_channel):
+        assert reliable_channel.ordering_scheme == "fifo_ordering"
+        self.seq = 0
+
+        # each processes' way of knowing what messages it has delivered
+        self.delivered = Queue()
+        self.reliable_channel = reliable_channel
+
+        # target is the callable object to be invoked in a separate thread on
+        # start()
+        self.listener = threading.Thread(target=self.listen)
+        self.listener.start()
+
+    def listen(self):
+        while True:
+            if self.reliable_channel.can_recv():
+                addr, msg = self.reliable_channel.recv()
+                self.delivered.put((addr, msg))
+
+    def recv(self):
+        if self.ordering_scheme == "causal_ordering":
+            addr, deliv_msg, vector = self.delivered.get()
+            self.reliable_channel.msg_vector[deliv_msg.id] += 1
+            return deliv_msg
+        elif self.ordering_scheme == "fifo_ordering":
+            return self.delivered.get()
+
+    def multicast(self, obj, group, vector, from_id):
+        # group is a list
+        self.seq += 1
+        msg = Message(self.seq, obj, from_id)
+        for addr in group:
+            self.reliable_channel.unicast(msg, addr, from_id)
